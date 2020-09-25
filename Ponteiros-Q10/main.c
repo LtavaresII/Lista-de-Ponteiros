dcd41c456d9159fa89c3b0028c65daf81eef89a5
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2020-09-25 11:57:54 -0300

    Add files via upload

---
+++ Ponteiros-Q10/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    char x[4];
+    //short int x[4];
+    //float x[4];
+    //double x[4];
+
+    printf("Valor do Endereco de x: %d \n", x);
+    printf("Valor do Endereco de x+1: %d \n", x+1);
+    printf("Valor do Endereco de x+2: %d \n", x+2);
+    printf("Valor do Endereco de x+3: %d \n", x+3);
+
+    return 0;
+}
